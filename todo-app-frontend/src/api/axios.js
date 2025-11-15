af8f5505833996b0336ae16577f397ab509a01c1
cuong081004 <2251120336@ut.edu.vn>
2025-11-15 18:08:56 +0700

    Update

--- todo-app-frontend/src/api/axios.js
+++ todo-app-frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', 
-    // baseURL: 'https://todo-app-t1g9.onrender.com/api', // ⚡ Dùng URL backend của bạn
+  // baseURL: 'http://localhost:5000/api', 
+  baseURL: 'https://todo-app-t1g9.onrender.com/api', // ⚡ Dùng URL backend của bạn
 });
 
 export default instance;
